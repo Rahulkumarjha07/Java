07abc391ec6bfc93f72a45abcc1998f317bdbabd
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-02-16 20:30:27 +0530

    committed succesfully

---
+++ trees/height.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class height{
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Tree {
+        int index = -1;
+
+        public Node build(int[] nodes) {
+            index++;
+
+            if (nodes[index] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[index]);
+
+            newNode.left = build(nodes);
+            newNode.right = build(nodes);
+
+            return newNode;
+        }
+
+      public int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh,rh)+1;
+      }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        
+        Tree t = new Tree();      // create object
+        Node root = t.build(nodes);
+        
+      int res =  t.height(root);
+      System.out.println(res);
+       
+    }
+}
